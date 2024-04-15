027869d7d727b39fd838d0d3c38da9e4438d032c
teddy7201 <henry10004@gmail.com>
2024-04-15 01:15:39 -0400

    Collisions In the Works
    The hockey puck and paddle are able to collide but the puck's velocity is out of control when the collision happens and isn't proportional to velocity change when it hit the paddle in a certain way. Still though, the chaos is fun to watch.

--- public/Scene1.js
+++ public/Scene1.js
@@ -1,21 +1,31 @@
+var velo = 100;
 class Scene1 extends Phaser.Scene
 {
-    block;
+    constructor(){
+        super("scene-game");
+        this.paddle;
+        this.puck;
+    }
 
     preload ()
     {
-        this.load.image('block', 'assets/sprites/block.png');
+        this.load.image('puck', '/../public/picsForHockey/puck.png');
+        this.load.image('paddle', '/../public/picsForHockey/hockey-paddle.png');
     }
 
     create ()
     {
-        this.block = this.physics.add.image(400, 100, 'block')
-            //.setVelocity(100, 200)
-            .setBounce(1, 1)
-            .setCollideWorldBounds(true);
+        this.paddle = this.physics.add.sprite(300, 400, 'paddle')
+        
+        this.paddle.setCollideWorldBounds(true);
+        this.paddle.setSize(150, 150, true);
+        this.paddle.setCircle(55, 0, 0);
+        this.paddle.setScale(1.2);
+        this.paddle.setPushable(false);
 
-        this.input.setDraggable(this.block.setInteractive());
 
+        this.input.setDraggable(this.paddle.setInteractive());
+        
         this.input.on('dragstart', (pointer, obj) =>
         {
             obj.body.moves = false;
@@ -30,18 +40,41 @@ class Scene1 extends Phaser.Scene
         {
             obj.body.moves = true;
         });
+
+        this.puck = this.physics.add.image(1000, 400, 'puck')
+        
+        this.puck.setVelocity(velo);
+        this.puck.setCollideWorldBounds(true);
+        this.puck.setBounce(1.1);
+        
+        this.puck.setCircle(55, 0, 0);
+        this.puck.setScale(1);
+
+        this.physics.add.collider(this.puck, this.paddle, this.updateVelo);
+        //this.physics.add.collider(this.paddle, this.puck);
+
+        
     }
+
+    update(){
+        if (this.physics.collide(this.paddle, this.puck)){
+            velo = velo + 75;
+            this.puck.setVelocity(velo);
+        }
+    }
+    
 }
+
+
 
 const config = {
     type: Phaser.WEBGL,
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     physics: {
         default: 'arcade',
         arcade: {
-            debug: true,
-            gravity: { y: 200 }
+            debug: true
         }
     },
     canvas: gameCanvas,
